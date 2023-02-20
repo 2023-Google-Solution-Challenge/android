336cc0a72842fd4057658884633fa424d3a2f7d5
chaeli <s.kcl5243@gmail.com>
2023-02-20 23:44:41 +0900

    FEAT wmain, apply, quit, recruit, contract activity

---
+++ app/src/main/java/com/example/signz_android/WMain/WQuitActivity.kt
@@ -0,0 +1,18 @@
+package com.example.signz_android.WMain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.signz_android.databinding.ActivityWQuitBinding
+
+class WQuitActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWQuitBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWQuitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //
+
+    }
+}
