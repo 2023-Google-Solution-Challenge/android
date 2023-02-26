c638e5dc4cb606ead9b7529eb8bd0c04d793bf6c
Sohee Yoo <20200867@sungshin.ac.kr>
2023-02-26 15:34:33 +0900

    FEAT 로그인, 회원가입

--- app/src/main/java/com/example/signz_android/LoginActivity.kt
+++ app/src/main/java/com/example/signz_android/LoginActivity.kt
@@ -1,11 +1,56 @@
 package com.example.signz_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.signz_android.databinding.ActivityLoginBinding
+import com.example.signz_android.databinding.ActivitySignupWorkerBinding
 
 class LoginActivity : AppCompatActivity() {
+    var DB = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.passIdFind.setOnClickListener {
+            val findIntent = Intent(this@LoginActivity, FindAccountActivity::class.java)
+            startActivity(findIntent)
+        }
+        binding.tvSignup.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
+        }
+
+        binding.btnSignin!!.setOnClickListener {
+            val email = binding.emailEdittext!!.text.toString()
+            val pass = binding.passwordEdittext!!.text.toString()
+            if (email == "" || pass == "") Toast.makeText(
+                this@LoginActivity,
+                "회원정보를 모두 입력해주세요",
+                Toast.LENGTH_SHORT
+            ).show() //else{
+//                val checkEmailpass = DB!!.checkEmailpass(email, pass)
+//                val checkisEmployer = DB!!.checkisEmployer
+//                if (checkEmailpass == true){
+//                    Toast.makeText(this@LoginActivity, "로그인 되었습니다.", Toast.LENGTH_SHORT)
+//                        .show()
+//                    if (checkisEmployer == 0){
+//                        val intent = Intent(applicationContext, MainActivity::class.java)
+//                        startActivity(intent)
+//                    } else {
+//                        val intent = Intent(applicationContext, MainActivity::class.java)
+//                        startActivity(intent)
+//                    }
+//                }
+//                else {
+//                    Toast.makeText(this@LoginActivity, " 회원정보가 존재하지 않습니다.", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//            }
+//        }
+
+        }
     }
 }
